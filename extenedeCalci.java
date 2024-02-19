67c22c2080ed6c9533e6c78e2c0890517d7ad858
Vineet13b <156497309+Vineet13b@users.noreply.github.com>
2024-02-19 14:39:18 +0530

    Update extenedeCalci.java

--- extenedeCalci.java
+++ extenedeCalci.java
@@ -1,3 +1,6 @@
+    
+    
+    
 public class extenedeCalci extends calci{
     int square(int a){
         int Sq = a * a;
@@ -8,6 +11,25 @@ public class extenedeCalci extends calci{
         int Cu = b * b * b;
         return Cu;
     }
+    public static void main(String args[]){
     
+       extenedeCalci c = new extenedeCalci();
+        float Add = c.sum(10,20);
+        float Sub = c.subs(10,20);
+        float Prod = c.prod(10,20);
+        float Div = c.div(10,20);
+        
+        System.out.println("Sum ="+Add);
+        System.out.println("difference ="+Sub);
+        System.out.println("Product ="+Prod);
+        System.out.println("Division ="+Div);
+        int Sqr = c.square(2);
+        System.out.println("Square No. ="+Sqr);
+        int Cube = c.cube(3);
+        System.out.println("Cube No. = "+Cube);
+        
+     
+    }
+
     
 }
