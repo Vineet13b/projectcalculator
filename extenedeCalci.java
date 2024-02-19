48ec6429e76870e5019e821da2552f0be86d4368
Vineet13b <156497309+Vineet13b@users.noreply.github.com>
2024-02-19 14:23:06 +0530

    Extended program for calci

---
+++ extenedeCalci.java
@@ -0,0 +1,13 @@
+public class extenedeCalci extends calci{
+    int square(int a){
+        int Sq = a * a;
+        return Sq;
+    }
+
+    int cube(int b){
+        int Cu = b * b * b;
+        return Cu;
+    }
+    
+    
+}
